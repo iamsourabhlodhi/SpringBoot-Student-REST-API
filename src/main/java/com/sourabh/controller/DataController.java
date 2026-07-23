b656d497af061e26cbd2726d93ee3b12ab7d2be9
Saurabh Lodhi <sourabhlodhi2318@gmail.com>
2026-07-23 18:00:49 +0530

    Add POST endpoint for creating records

--- src/main/java/com/sourabh/controller/DataController.java
+++ src/main/java/com/sourabh/controller/DataController.java
@@ -1,6 +1,6 @@
 package com.sourabh.controller;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,13 +8,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.sourabh.model.Data;
 import com.sourabh.service.DataService;
 
 @RestController
 public class DataController {
-@Autowired
-DataService dataservice;
+final DataService dataservice;
+
+	DataController(DataService dataservice) {
+		this.dataservice = dataservice;
+	}
 
 @GetMapping("/getall")
 public List<Data> getall(){
